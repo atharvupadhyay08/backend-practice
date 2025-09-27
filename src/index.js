6a226a22889d6085a08315bb1d44459fee4328a1
atharvupadhyay08 <upadhyayatharv008@gmail.com>
2025-09-27 23:27:04 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 dotenv.config()
-
+import {app} from "./app.js"
 
 import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
@@ -11,6 +11,19 @@ console.log("Mongo URL is:", process.env.MONGODB_URI);
 
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`🚀 Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("❌ MongoDB connection failed !!!", error);
+  });
 
 /*
 import express from "express"
